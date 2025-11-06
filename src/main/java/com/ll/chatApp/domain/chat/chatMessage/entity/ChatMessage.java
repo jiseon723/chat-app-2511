8197d645ab2e21db9ca2e1c334f460ac719656a3
jiseon723 <dighxxx12@gmail.com>
2025-11-06 15:30:36 +0900

    entity

--- src/main/java/com/ll/chatApp/domain/chat/chatMessage/entity/ChatMessage.java
+++ src/main/java/com/ll/chatApp/domain/chat/chatMessage/entity/ChatMessage.java
@@ -1,7 +1,9 @@
 package com.ll.chatApp.domain.chat.chatMessage.entity;
 
+import com.ll.chatApp.domain.chat.chatRoom.entity.ChatRoom;
 import com.ll.chatApp.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,5 +16,8 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @ToString(callSuper = true)
 public class ChatMessage extends BaseEntity {
-
+    private String writerName;
+    private String content;
+    @ManyToOne
+    private ChatRoom chatRoom;
 }
--- src/main/java/com/ll/chatApp/domain/chat/chatRoom/entity/ChatRoom.java
+++ src/main/java/com/ll/chatApp/domain/chat/chatRoom/entity/ChatRoom.java
@@ -1,11 +1,15 @@
 package com.ll.chatApp.domain.chat.chatRoom.entity;
 
+import com.ll.chatApp.domain.chat.chatMessage.entity.ChatMessage;
 import com.ll.chatApp.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,5 +17,6 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @ToString(callSuper = true)
 public class ChatRoom extends BaseEntity {
-
+    @OneToMany
+    public List<ChatMessage> chatMessages;
 }
