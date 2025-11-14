48a847f444c471c248846c07f73afe34a6bf8354
jiseon723 <dighxxx12@gmail.com>
2025-11-14 15:45:19 +0900

    kw 3

--- src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepository.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    Page<Article> findByTitleContainingOrContentContaining(String kw, String kw_, Pageable pageable);
+    Page<Article> findByAuthor_usernameOrTitleContainingOrContentContaining(String kw, String kw_, String kw__, Pageable pageable);
 
     Page<Article> findByTitleContaining(String kw, Pageable pageable);
 
     Page<Article> findByContentContaining(String kw, Pageable pageable);
+
+    Page<Article> findByAuthor_usernameContaining(String kw, Pageable pageable);
 }
