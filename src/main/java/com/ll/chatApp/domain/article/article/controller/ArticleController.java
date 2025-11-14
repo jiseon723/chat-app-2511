26c0c878de8597cf98bed583ded17fb9db07a0aa
jiseon723 <dighxxx12@gmail.com>
2025-11-14 15:31:32 +0900

    kw 2

--- src/main/java/com/ll/chatApp/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/chatApp/domain/article/article/controller/ArticleController.java
@@ -45,7 +45,9 @@ public class ArticleController {
                 ));
 
         log.debug("kwTypesMap : {}", kwTypesMap);
-        Page<Article> itemsPage = articleService.search(pageable);
+
+        Page<Article> itemsPage = articleService.search(kwTypes, kw, pageable);
+
         model.addAttribute("itemsPage", itemsPage);
         model.addAttribute("kwTypesMap", kwTypesMap);
         return "article/list";
--- src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepository.java
@@ -1,10 +1,16 @@
 package com.ll.chatApp.domain.article.article.repository;
 
 import com.ll.chatApp.domain.article.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    Page<Article> findByTitleContainingOrContentContaining(String kw, String kw_, Pageable pageable);
+
+    Page<Article> findByTitleContaining(String kw, Pageable pageable);
+
+    Page<Article> findByContentContaining(String kw, Pageable pageable);
 }
