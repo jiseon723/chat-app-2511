469324ec0ae08e0285279f72b1c67e11da66b5cd
jiseon723 <dighxxx12@gmail.com>
2025-11-17 14:20:30 +0900

    query dsl

--- src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepository.java
@@ -1,18 +1,12 @@
 package com.ll.chatApp.domain.article.article.repository;
 
 import com.ll.chatApp.domain.article.article.entity.Article;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-    Page<Article> findByAuthor_usernameContainingOrTitleContainingOrContentContainingOrTags_contentContainingOrComments_author_usernameContainingOrComments_bodyContaining(String kw, String kw1, String kw2, String kw3, String kw4, String kw5, Pageable pageable);
-
-    Page<Article> findByTitleContaining(String kw, Pageable pageable);
-
-    Page<Article> findByContentContaining(String kw, Pageable pageable);
-
-    Page<Article> findByAuthor_usernameContaining(String kw, Pageable pageable);
+public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleRepositoryCustom {
+    List<Article> findByOrderByIdDesc();
 }
